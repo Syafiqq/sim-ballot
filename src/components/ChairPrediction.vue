8d04f4c83cbe94a4cf445f722f24715e02baaf1c
Syafiq <syafiq.rezpector@gmail.com>
2019-01-20 20:58:16 +0700

    Fix fileds

--- src/components/ChairPrediction.vue
+++ src/components/ChairPrediction.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
-  b-table(responsive='', bordered='', outlined='', footClone='', hover='', :items='items', :fields='sFields')
+  b-table(responsive='', bordered='', outlined='', hover='', :items='items', :fields='sFields')
 </template>
 
 <script>
+import {ConfigRepository} from "@/resources/js/repositories/config-repository";
+
 const items = [];
 
 export default {
@@ -10,11 +12,33 @@ export default {
   data () {
     return {
       items: items,
-      fields: [
+      fields: []
+    }
+  },
+  created () {
+    this.fetch()
+  },
+  methods: {
+    async fetch () {
+      const {num_ranks} = await ConfigRepository.get();
+      this.mergeFields(num_ranks);
+    },
+    mergeFields (ranks) {
+      let vm = this;
+      let fields = [
         {key: 'no', label: 'No', thStyle: {width: '50px'}},
         {key: 'party', label: 'Partai', thStyle: {width: '150px'}},
         {key: 'ballot', label: 'Suara', thStyle: {width: '100px'}}
-      ]
+      ];
+      window._.forEach(window._.range(1, ranks + 1), function (value) {
+        fields.push({key: `r.${value}`, label: `R${value * 2 - 1}`, thStyle: {width: '80px'}})
+      });
+      while (vm.fields.length > 0) {
+        vm.fields.pop();
+      }
+      window._.forEach(fields, function (value) {
+        vm.fields.push(value);
+      })
     }
   },
   computed: {
