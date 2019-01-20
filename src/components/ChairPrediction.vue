da412a3da346c67208ac1fa6cceffce87bd952d4
Syafiq <syafiq.rezpector@gmail.com>
2019-01-20 19:21:35 +0700

    Add dummy example

--- src/components/ChairPrediction.vue
+++ src/components/ChairPrediction.vue
@@ -1,11 +1,22 @@
-<template>
-
+<template lang="pug">
+  b-table(responsive='', bordered='', outlined='', fixed='', footClone='', hover='', :items='items')
 </template>
 
 <script>
+const items = [
+  {isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald'},
+  {isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw'},
+];
+
 export default {
-  name: "ChairPrediction"
+  name: "ChairPrediction",
+  data () {
+    return {
+      items: items
+    }
+  }
 }
+
 </script>
 
 <style scoped>
