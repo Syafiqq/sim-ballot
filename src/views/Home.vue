a3fc0ec93d15fb1ab10af7e0df8737659e6f8fb5
Syafiq <syafiq.rezpector@gmail.com>
2019-01-20 19:11:18 +0700

    Add Chair Prediction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template lang="pug">
   .home
+    chair-prediction
 </template>
 
 <script>
 // @ is an alias to /src
+import ChairPrediction from '@/components/ChairPrediction.vue'
 
 export default {
   name: 'home',
   components: {
+    ChairPrediction
   }
 }
 </script>
