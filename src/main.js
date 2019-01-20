4dca286a5e74e2dc3132442b0b35e1f4adc81e29
Syafiq <syafiq.rezpector@gmail.com>
2019-01-20 15:46:49 +0700

    Register plain container

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PlainContainer from './containers/PlainContainer.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component('plain-container', PlainContainer);
+
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
