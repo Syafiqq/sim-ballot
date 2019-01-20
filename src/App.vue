811fca03d8b336efe5d5f3bb20f56499ed24cbbc
Syafiq <syafiq.rezpector@gmail.com>
2019-01-20 15:36:48 +0700

    Fix app vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    component(:is='layout')
+      transition
+        router-view
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+const defaultLayout = 'plain-container';
+
+export default {
+  name: 'app',
+  components: {},
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {
+    layout () {
+      return (this.$route.meta.layout || defaultLayout)
     }
+  },
+
+  mounted () {
+  },
+
+  created () {
+    // nothing defined here (when this.$route.path is other than "/")
+    // console.log({ route: this.$route, meta: this.$route.meta.layout })
+  },
+
+  updated () {
+    // something defined here whatever the this.$route.path
+    // console.log({ route: this.$route, meta: this.$route.meta.layout })
   }
 }
+</script>
+
+
+<style lang="scss">
+
 </style>
+
