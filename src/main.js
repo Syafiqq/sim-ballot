7188257be4a95514c42a4cb28dae4036b26ce43d
Syafiq <syafiq.rezpector@gmail.com>
2019-01-20 15:50:04 +0700

    Add Bootstrap vue

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import PlainContainer from './containers/PlainContainer.vue'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false;
 Vue.component('plain-container', PlainContainer);
+Vue.use(BootstrapVue);
 
 
 new Vue({
