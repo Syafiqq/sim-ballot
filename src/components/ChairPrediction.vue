20b63f66745c184a30350f449625082373fab7ce
Syafiq <syafiq.rezpector@gmail.com>
2019-01-22 04:28:38 +0700

    Add items non loop

--- src/components/ChairPrediction.vue
+++ src/components/ChairPrediction.vue
@@ -19,8 +19,9 @@ export default {
   },
   methods: {
     async fetch () {
-      const {num_ranks} = await ConfigRepository.get();
+      const {parties, num_ranks} = await ConfigRepository.get();
       this.mergeFields(num_ranks);
+      this.createParties(parties);
     },
     mergeFields (ranks) {
       let vm = this;
@@ -29,14 +30,27 @@ export default {
         {key: 'party', label: 'Partai', thStyle: {width: '150px'}},
         {key: 'ballot', label: 'Suara', thStyle: {width: '100px'}}
       ];
-      window._.forEach(window._.range(1, ranks + 1), function (value) {
+      window._.forEach(window._.range(1, ranks + 1), (value) => {
         fields.push({key: `r.${value}`, label: `R${value * 2 - 1}`, thStyle: {width: '80px'}})
       });
       while (vm.fields.length > 0) {
         vm.fields.pop();
       }
-      window._.forEach(fields, function (value) {
+      window._.forEach(fields, (value) => {
         vm.fields.push(value);
+      })
+    },
+    createParties (parties) {
+      let vm = this;
+      while (vm.items.length > 0) {
+        vm.items.pop();
+      }
+      window._.forEach(parties, (v, k) => {
+        vm.items.push({
+          no: k + 1,
+          party: v,
+          ballot: 0
+        })
       })
     }
   },
