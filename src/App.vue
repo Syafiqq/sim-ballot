9f98cc3fab5f4772485093c6ea18f93f1eb0f2ac
Syafiq <syafiq.rezpector@gmail.com>
2019-01-26 13:48:03 +0700

    directly load home

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,13 @@
 <template lang="pug">
-  #app
-    component(:is='layout')
-      transition
-        router-view
+  home
 </template>
 
 <script>
-const defaultLayout = 'plain-container';
+import Home from "@/views/Home";
 
 export default {
   name: 'app',
-  components: {},
-  data () {
-    return {}
-  },
-  methods: {},
-  computed: {
-    layout () {
-      return (this.$route.meta.layout || defaultLayout)
-    }
-  },
-
-  mounted () {
-  },
-
-  created () {
-    // nothing defined here (when this.$route.path is other than "/")
-    // console.log({ route: this.$route, meta: this.$route.meta.layout })
-  },
-
-  updated () {
-    // something defined here whatever the this.$route.path
-    // console.log({ route: this.$route, meta: this.$route.meta.layout })
-  }
+  components: {Home},
 }
 </script>
 
