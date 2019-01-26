ba0e6740a781ff4ab5a9488cf28fcb465991f2d4
Syafiq <syafiq.rezpector@gmail.com>
2019-01-26 12:25:32 +0700

    Add window idt

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -2,3 +2,4 @@ window._ = require('lodash');
 var Excel = require('exceljs/dist/es5/exceljs.browser');
 window.ExcelJS = Excel;
 window.FileSaver = require('file-saver');
+window.IDT = require('./idTranslator').IDTranslator;
