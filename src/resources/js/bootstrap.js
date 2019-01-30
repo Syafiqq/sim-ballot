444178349ae013eaaeccef9f3cff886a0ce606e8
Syafiq <syafiq.rezpector@gmail.com>
2019-01-30 07:11:05 +0700

    Register pdf make

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -3,3 +3,6 @@ var Excel = require('exceljs/dist/es5/exceljs.browser');
 window.ExcelJS = Excel;
 window.FileSaver = require('file-saver');
 window.IDT = require('./idTranslator').IDTranslator;
+window.pdfMake = require('pdfmake/build/pdfmake.js');
+window.pdfFonts = require('pdfmake/build/vfs_fonts.js');
+window.pdfMake.vfs = window.pdfFonts.pdfMake.vfs;
